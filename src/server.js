a39841e03328887e94d71ad72d988cd6c1aad800
Emma Kennelly <emma.f.kennelly.19@dartmouth.edu>
2018-06-01 09:25:58 -0400

    done

--- src/server.js
+++ src/server.js
@@ -3,10 +3,38 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import botkit from 'botkit';
 
+const yelp = require('yelp-fusion');
+
+dotenv.config({ silent: true });
+
+// and then the secret is usable this way:
 // initialize
 const app = express();
+// botkit controller
+const controller = botkit.slackbot({
+  debug: false,
+});
 
+// initialize slackbot
+const slackbot = controller.spawn({
+  token: process.env.SLACK_BOT_TOKEN,
+  // this grabs the slack token we exported earlier
+}).startRTM((err) => {
+  // start the real time message client
+  if (err) { throw new Error(err); }
+});
+
+
+// prepare webhook
+// for now we won't use this but feel free to look up slack webhooks
+controller.setupWebserver(process.env.PORT || 3001, (err, webserver) => {
+  controller.createWebhookEndpoints(webserver, slackbot, () => {
+    if (err) { throw new Error(err); }
+  });
+});
 // enable/disable cross origin resource sharing if necessary
 app.use(cors());
 
@@ -38,3 +66,34 @@ const port = process.env.PORT || 9090;
 app.listen(port);
 
 console.log(`listening on: ${port}`);
+
+// example hello response
+controller.hears(['food'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  const yelpClient = yelp.client(process.env.YELP_CLIENT_SECRET);
+
+  yelpClient.search({
+    term: 'Sushi',
+    location: 'hanover, nh',
+  }).then((response) => {
+    console.log(response.jsonBody.businesses[0].name);
+    response.jsonBody.businesses.forEach((business) => {
+      bot.reply(message, business.name);
+    });
+  }).catch((e) => {
+    console.log(e);
+  });
+});
+
+controller.on('user_typing', (bot, message) => {
+  bot.reply(message, 'stop typing!');
+});
+
+controller.hears(['hello', 'hi', 'howdy'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  bot.api.users.info({ user: message.user }, (err, res) => {
+    if (res) {
+      bot.reply(message, `Hello, ${res.user.name}!`);
+    } else {
+      bot.reply(message, 'Hello there!');
+    }
+  });
+});
